2046d743baaf1891416180c381423f9e385bdcce
motypes <mohammed.jalil@gmail.com>
2024-03-28 11:51:18 +0800

    complete job manager

--- backend/job_manager.py
+++ backend/job_manager.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from datetime import datetime
-from typing import List
+from threading import Lock
+from typing import Dict, List
 
 
 @dataclass
@@ -12,3 +13,22 @@ class Event:
 class Job:
     status: str
     events: List[Event]
+
+jobs_lock = Lock()
+jobs: Dict[str, 'Job'] = {}
+
+def append_event(job_id: str, event_data: str):
+    with jobs_lock:
+        if job_id in jobs:
+            jobs[job_id] = Job(
+                status='STARTED',
+                events=[],
+                result=""
+            )
+        else:
+            print(f'Appending for job {job_id}')
+            jobs[job_id].events.append(
+                Event(
+                timestamp=datetime.now(),
+                data=event_data
+            ))
