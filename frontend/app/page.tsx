25358e299f7d1f8587106216fe0a8cd40bdea175
motypes <mohammed.jalil@gmail.com>
2024-04-02 13:10:33 +0800

    wip agent usehook

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import InputSection from "@/components/InputSection";
+import useCrewJob from "@/hooks/useCrewJob";
 import { useState } from "react";
 
 export default function Home() {
-  const [companies, setCompanies] = useState<string[]>([]);
-  const [positions, setPositions] = useState<string[]>([]);
+  const crewJob = useCrewJob();
+  const { positions, setPositions, companies, setCompanies } = crewJob;
   return (
     <div className="bg-white min-h-screen text-black">
       <div className="flex">
@@ -24,7 +25,16 @@ export default function Home() {
             setData={setPositions}
           />
         </div>
-        <div></div>
+        <div className="w-1/2 p-4 flex flex-col">
+          <div className=" justify-between flex items-center mb-4">
+            <h2 className="text-2xl font-bold">Output</h2>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 rounded-lg px-4">
+              Start
+            </button>
+          </div>
+          {/* FINAL OUTPUT */}
+          {/* EVENT LOG */}
+        </div>
       </div>
     </div>
   );
---
+++ frontend/hooks/useCrewJob.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+type EventType = {
+    data: string;
+    timestamp: string;
+ }
+
+export type NamedUrls = {
+    name: string;
+    url: string[];
+}
+
+export type PositionInfo = {
+    company: string;
+    position: string;
+    name: string;
+    blog_articles: string[];
+    youtube_interviews: NamedUrls[];
+}
+
+export type PositionInfoList = {
+    positions: PositionInfo[];
+}
+
+function useCrewJob() {
+    const [companies, setCompanies] = useState<string[]>([]);
+    const [positions, setPositions] = useState<string[]>([]);
+    const [events, setEvents] = useState<EventType[]>([]);
+    const [positionInfo, setPositionInfo] = useState<PositionInfoList[]>([]);
+    const [running, setRunning] = useState<boolean>(false);
+    const [currentJobId, setCurrentJobId] = useState<string>("");
+
+    const startJob = async () => {
+        // clean up the old job
+        setEvents([]);
+        setPositionInfo([]);
+        setRunning(true);
+
+        // request to our backend
+
+        // update out state
+    }
+  return {
+    companies,
+    setCompanies,
+    positions,
+    setPositions,
+  }
+}
+
+export default useCrewJob
